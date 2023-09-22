8bce69aac48e0ff0fe3f4784131e9d12875ca44e
misc4747 <misc4747@gmail.com>
2023-09-22 03:48:34 +0000

    refactor: format flask-docker-ps.py

--- flask-docker-ps.py
+++ flask-docker-ps.py
@@ -3,6 +3,7 @@ import docker
 
 app = Flask(__name__)
 
+
 class container_info:
     def __init__(self, name, image, state, status):
         self.name = name
@@ -10,22 +11,28 @@ class container_info:
         self.state = state
         self.status = status
 
-@app.route('/')
+
+@app.route("/")
 def index():
     # Dockerクライアントの初期化
-    client = docker.APIClient(base_url='unix://var/run/docker.sock')
+    client = docker.APIClient(base_url="unix://var/run/docker.sock")
 
     # 実行中のコンテナの一覧を取得
     containers = client.containers()
     container_status_list = []
     for container in containers:
-        info = container_info(container["Names"][0], container["Image"], container["State"], container["Status"])
+        info = container_info(
+            container["Names"][0],
+            container["Image"],
+            container["State"],
+            container["Status"],
+        )
         print(container["Names"][0])
         container_status_list.append(info)
-        
-    # コンテナ情報をテンプレートに渡す
-    return render_template('index.html', containers=container_status_list)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
-                            
+    # コンテナ情報をテンプレートに渡す
+    return render_template("index.html", containers=container_status_list)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
