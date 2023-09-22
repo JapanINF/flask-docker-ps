e4b7509ea48c151a476f49295a3651400c2a8852
misc4747 <misc4747@gmail.com>
2023-09-22 03:33:36 +0000

    add: display uptime

--- flask-docker-ps.py
+++ flask-docker-ps.py
@@ -3,16 +3,28 @@ import docker
 
 app = Flask(__name__)
 
+class container_info:
+    def __init__(self, name, image, state, status):
+        self.name = name
+        self.image = image
+        self.state = state
+        self.status = status
+
 @app.route('/')
 def index():
     # Dockerクライアントの初期化
-    client = docker.from_env()
+    client = docker.APIClient(base_url='unix://var/run/docker.sock')
 
     # 実行中のコンテナの一覧を取得
-    containers = client.containers.list()
-
+    containers = client.containers()
+    container_status_list = []
+    for container in containers:
+        info = container_info(container["Names"][0], container["Image"], container["State"], container["Status"])
+        print(container["Names"][0])
+        container_status_list.append(info)
+        
     # コンテナ情報をテンプレートに渡す
-    return render_template('index.html', containers=containers)
+    return render_template('index.html', containers=container_status_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
